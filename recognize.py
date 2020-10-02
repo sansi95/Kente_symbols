3e9a4e342c4cd9509c926cc597e27ab3d938d240
sansitha <sansitha@umich.edu>
2020-10-02 17:02:22 -0400

    rrecognize

---
+++ recognize.py
@@ -0,0 +1,68 @@
+# import the necessary packages
+from pyimagesearch.localbinarypatterns import LocalBinaryPatterns
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from imutils import paths
+import argparse
+import cv2
+import os
+import numpy as np
+from sklearn.linear_model import SGDRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+l = []
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--training", required=True,
+	help="path to the training images")
+ap.add_argument("-e", "--testing", required=True,
+	help="path to the tesitng images")
+args = vars(ap.parse_args())
+# initialize the local binary patterns descriptor along with
+# the data and label lists
+desc = LocalBinaryPatterns(24, 8)
+data = []
+labels = []
+
+# loop over the training images
+for imagePath in paths.list_images(args["training"]):
+	# load the image, convert it to grayscale, and describe it
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	hist = desc.describe(gray)
+	# extract the label from the image path, then update the
+	# label and data lists
+	labels.append(imagePath.split(os.path.sep)[-2])
+	data.append(hist)
+# model = LinearSVC(C=100.0, random_state=42)
+model = DecisionTreeClassifier(random_state=0)
+# rng = np.random.RandomState(0)
+# model = make_pipeline(StandardScaler(),SGDRegressor(max_iter=1000, tol=1e-3))
+model.fit(data, labels)
+
+# loop over the testing images
+for imagePath in paths.list_images(args["testing"]):
+	# load the image, convert it to grayscale, describe it,
+	# and classify it
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	hist = desc.describe(gray)
+	prediction = model.predict(hist.reshape(1, -1))
+
+	# display the image and the prediction
+	# cv2.putText(image, prediction[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+	# 	1.0, (0, 0, 255), 3)
+	# cv2.imshow("Image", image)
+	# cv2.waitKey(0)
+	l.append([prediction[0], imagePath])
+
+print (l)
+
+from symbol import symbol
+
+for i in l:
+	print (symbol(str(i[0])))
